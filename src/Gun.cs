18ee17569ac23d9d5375cf24cdb142e44e17aafa
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-12-10 15:22:10 +0900

    feat: add gun

---
+++ src/Gun.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Gun : CharacterBody2D
+{
+	public override void _PhysicsProcess(double delta)
+    {
+        LookAt(GetGlobalMousePosition());
+        MoveAndSlide();
+    }
+}
