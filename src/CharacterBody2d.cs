be979d629793702d6e8fa5b67b2d3f7f55fc486e
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-12-10 15:34:27 +0900

    refactor: player code

--- src/CharacterBody2d.cs
+++ src/CharacterBody2d.cs
@@ -14,27 +14,19 @@ public partial class CharacterBody2d : CharacterBody2D
 
 		// Add the gravity.
 		if (!IsOnFloor())
-		{
 			velocity += GetGravity() * (float)delta;
-		}
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-		{
 			velocity.Y = JumpVelocity;
-		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("left", "right", "jump", "squat");
 		if (direction != Vector2.Zero)
-		{
 			velocity.X = direction.X * Speed;
-		}
 		else
-		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
 
 		Velocity = velocity;
 		MoveAndSlide();
